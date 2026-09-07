02b563e014174ccdf289b483dde64a399b3237fb
DylanDominguez <dylandomperez1802@gmail.com>
2026-09-07 11:52:14 -0500

    CRUD en las rutas de la api

--- app/database.py
+++ app/database.py
@@ -1,9 +1,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from fastapi import Depends
+from database import SessionmLocal
 
 #url que define la conexion a la base de datos
-DATABASE_URL = "mysql+pymysql://root:admin@localhost:3306/fastapi_crud"
+DATABASE_URL = "mysql+pymysql://root:admin@localhost:3306/ecommerce_db"
 
 engine = create_engine(DATABASE_URL)
 session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def obtener_db():
+    db = SessionmLocal()
+    try:
+        yield db
+    finally:
+        db.close()
---
+++ app/models.py
@@ -0,0 +1,30 @@
+from sqlalchemy import Column, Integer, String, Float, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
+from database import Base
+
+# Creación de la clase Categoria para definir sus atributos en la BD
+class Categoria(Base):
+    __tablename__ = "categorias"
+    id = Column(Integer, primary_key=True, index=True)
+    # El nombre de la categoría debe ser único
+    nombre = Column(String(100), unique=True, index=True)
+    # Relación 1:M con Producto
+    productos = relationship("Producto", back_populates="categoria")
+    #back_populates="categoria" va a hacer referencia al atributo categoria dentro de Productos
+
+class Producto(Base):
+    __tablename__ = "productos"
+    id = Column(Integer, primary_key=True, index=True)
+    nombre = Column(String(100), index=True)
+    precio = Column(Float)
+    # Por defecto, el producto tiene stock
+    en_stock = Column(Boolean, default=True)
+    categoria_id = Column(Integer, ForeignKey("categorias.id"))
+    categoria = relationship("Categoria", back_populates="productos")
+
+class Usuario(Base):
+    __tablename__ = "usuarios"
+    id = Column(Integer, primary_key=True, index=True)
+    nombre = Column(String(100), unique=True, index=True)
+    email = Column(String(150), unique=True, index=True)
+    hashed_password = Column(String(255))
